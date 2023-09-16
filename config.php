405827e228f79f85e7c352f37984442097468c97
tomarnold447 <tomarnold447@gmail.com>
2023-09-16 13:05:47 +0300

    Able to post contact data to database

--- config.php
+++ config.php
@@ -1,5 +1,31 @@
 <?php
+$hostName = "localhost";
+$dbUser = "root";
+$dbPassword = "";
+$dbName = "contacts";
 
-$conn = mysqli_connect('localhost','root','','login_register');
+$conn = mysqli_connect($hostName, $dbUser, $dbPassword, $dbName);
 
+if (!$conn) {
+    die("Something went wrong;");
+}
+
+$isMessageSent = false;
+
+if(isset($_POST['button'])) {
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $message = $_POST['message'];
+
+    $query = "INSERT INTO contacts_db(name,email,message)VALUES('$name','$email','$message')";
+
+    if(mysqli_query($conn, $query)){
+        echo "Success";
+        $isMessageSent = true;
+        header('location:index.php');
+    } else {
+        $isMessageSent = false;
+        echo "Error".mysqli_error($conn);
+    }
+}
 ?>
