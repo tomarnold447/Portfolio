0289bd2ce0800444eaec6927f2948f8558bc8807
tomarnold447 <tomarnold447@gmail.com>
2024-04-09 03:18:15 +0300

    Initial commit

--- api/php-handler.js
+++
@@ -1,8 +0,0 @@
-const { createServer } = require('http');
-const { parse } = require('url');
-const { exec } = require('child_process');
-
-createServer((req, res) => {
-    const { query } = parse(req.url, true);
-    
-}).listen();
